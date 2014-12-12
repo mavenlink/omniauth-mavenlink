cc57a435766869d3ded3f0e22f3e94ea7370fd66
Adam Bedford <adam.bedford@live.com>
2014-12-12 21:10:52 +0000

    updates

--- lib/omniauth/strategies/mavenlink.rb
+++ lib/omniauth/strategies/mavenlink.rb
@@ -3,27 +3,30 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
   	class Mavenlink < OmniAuth::Strategies::OAuth2
-  		option :client_options, {
+  		
+      option :name, 'mavenlink'
+
+      option :client_options, {
   			site: 'https://app.mavenlink.com',
-  			authorize_url: 'https://app.mavenlink.com/oauth/authorize',
+  			authorize_url: 'https://app.mavenlink.com/oauth/authorize?response_type=code',
   			token_url: 'https://app.mavenlink.com/oauth/token'
   		}
 
-  		def request_phase
-        	super
-	    end
+  		# def request_phase
+    #     	super
+	   #  end
 	      
-	    def authorize_params
-	    	super.tap do |params|
-	        	%w[scope client_options].each do |v|
-		            if request.params[v]
-		              params[v.to_sym] = request.params[v]
-		            end
-	        	end
-	        end
+	    # def authorize_params
+	    # 	super.tap do |params|
+	    #     	%w[scope client_options].each do |v|
+		   #          if request.params[v]
+		   #            params[v.to_sym] = request.params[v]
+		   #          end
+	    #     	end
+	    #     end
 
-          session['omniauth.state'] = params[:state] if params['state']
-	    end
+     #      session['omniauth.state'] = params[:state] if params['state']
+	    # end
 
 	    uid { raw_info['id'].to_s }
 
@@ -38,25 +41,44 @@ module OmniAuth
     	  }
     	end
 
+
+
+      alias :oauth2_access_token :access_token
+
+      def access_token
+        ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
+          :mode => :query,
+          :param_name => 'oauth2_access_token',
+          :expires_in => oauth2_access_token.expires_in,
+          :expires_at => oauth2_access_token.expires_at
+        })
+      end
+      
+
     	def raw_info
     		@raw_info ||= access_token.get('https://api.mavenlink.com/api/v1/users/me.json').parsed
     	end
 
 
-      def custom_build_access_token
-        if request.xhr? && request.params['code']
-          verifier = request.params['code']
-          client.auth_code.get_token(verifier, { :redirect_uri => '/sessions/new'}.merge(token_params.to_hash(:symbolize_keys => true)),
-                                     deep_symbolize(options.auth_token_params || {}))
-        # elsif verify_token(request.params['id_token'], request.params['access_token'])
-        elsif request.params['id_token'] && request.params['access_token']
-          ::OAuth2::AccessToken.from_hash(client, request.params.dup)
-        else
-          orig_build_access_token
-        end
-      end
-      alias_method :orig_build_access_token, :build_access_token
-      alias_method :build_access_token, :custom_build_access_token
+      # def custom_build_access_token
+      #   if request.xhr? && request.params['code']
+      #     verifier = request.params['code']
+      #     client.auth_code.get_token(verifier, { :redirect_uri => '/sessions/new'}.merge(token_params.to_hash(:symbolize_keys => true)),
+      #                                deep_symbolize(options.auth_token_params || {}))
+      #   # elsif verify_token(request.params['id_token'], request.params['access_token'])
+      #   elsif request.params['id_token'] && request.params['access_token']
+      #     ::OAuth2::AccessToken.from_hash(client, request.params.dup)
+      #   else
+      #     orig_build_access_token
+      #   end
+      # end
+      # alias_method :orig_build_access_token, :build_access_token
+      # alias_method :build_access_token, :custom_build_access_token
+
+
+
+
+
 
 
   	end
