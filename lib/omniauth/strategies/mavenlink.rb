80be8769d3d056050e48e3ded4239a62da31f60c
Adam Bedford <adam.bedford@live.com>
2014-12-12 20:52:43 +0000

    updates

--- lib/omniauth/strategies/mavenlink.rb
+++ lib/omniauth/strategies/mavenlink.rb
@@ -20,7 +20,9 @@ module OmniAuth
 		              params[v.to_sym] = request.params[v]
 		            end
 	        	end
-	        end
+	      end
+
+        session['omniauth.state'] = params[:state] if params['state']
 	    end
 
 	    uid { raw_info['id'].to_s }
@@ -32,7 +34,9 @@ module OmniAuth
     	end
 
     	extra do {
-        	'raw_info' => raw_info
+        hash = {}
+        hash[:id_token] = access_token['id_token']
+        hash[:raw_info] = raw_info unless skip_info?
     	  }
     	end
 
