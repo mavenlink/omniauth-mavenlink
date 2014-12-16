9393d11f121883fb980688f0090f75e8b32d5bd2
Adam Bedford <adam.bedford@live.com>
2014-12-16 01:00:00 +0000

    updates

--- lib/omniauth/strategies/mavenlink.rb
+++ lib/omniauth/strategies/mavenlink.rb
@@ -1,4 +1,5 @@
 require 'omniauth-oauth2'
+require 'json'
 
 module OmniAuth
   module Strategies
@@ -20,7 +21,7 @@ module OmniAuth
 
 	    info do {
         	:name => raw_info['users']['5207615']['full_name'],
-        	:email => raw_info['users']['email_address'],
+        	:email => raw_info['users'].first { |user| do user['email_address'] },
           :plan_type => raw_info['users']['plan_type'],
           :account_permission => raw_info['users']['account_permission']
           # name: raw_info
@@ -28,12 +29,14 @@ module OmniAuth
     	end
 
     	extra do {
-        	'raw_info' => raw_info['users']
+        	'raw_info' => raw_info
     	  }
     	end
 
     	def raw_info
-    		@raw_info ||= access_token.get('https://api.mavenlink.com/api/v1/users/me.json').parsed
+    		# @raw_info ||= access_token.get('https://api.mavenlink.com/api/v1/users/me.json').parsed
+        parse = JSON.parse(access_token.get('https://api.mavenlink.com/api/v1/users/me.json'))
+        @raw_info ||= parse['users'].values.first
     	end
 
       # def user_id
